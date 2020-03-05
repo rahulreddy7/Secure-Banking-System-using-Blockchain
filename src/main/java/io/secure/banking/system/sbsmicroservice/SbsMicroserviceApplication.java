6c6e294e9abfec8756e14013d6e02cb370a3a6e8
Devansh Sharma <devansh@Devanshs-MacBook-Pro.local>
2020-03-05 03:26:34 -0700

    initial code setup

---
+++ src/main/java/io/secure/banking/system/sbsmicroservice/SbsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.secure.banking.system.sbsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/secure/banking/system/sbsmicroservice/SbsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.secure.banking.system.sbsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
