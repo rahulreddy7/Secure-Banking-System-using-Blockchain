c6957b0a9e6eeb92c2fde17d3259df2799815520
Akshay Shah <akshay.vjti11@gmail.com>
2020-03-14 17:20:01 -0700

    fixing autowiring issues

--- src/main/java/io/secure/banking/system/sbsmicroservice/SbsMicroserviceApplication.java
+++ src/main/java/io/secure/banking/system/sbsmicroservice/SbsMicroserviceApplication.java
@@ -1,16 +1,20 @@
 package io.secure.banking.system.sbsmicroservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.SpringVersion;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan({"io.sbs.*"})
 @EnableMongoRepositories("io.sbs.*")
+@EnableAutoConfiguration()
 public class SbsMicroserviceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("version: " + SpringVersion.getVersion());
 		SpringApplication.run(SbsMicroserviceApplication.class, args);
 	}
 
