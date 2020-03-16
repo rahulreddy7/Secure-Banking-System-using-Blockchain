769ede58e1622a9637428bb095acdd846538e443
Anay Paul <anay.paul2@gmail.com>
2020-03-16 11:22:28 -0700

    Update ResultStatus.java

--- src/main/java/io/sbs/constant/ResultStatus.java
+++ src/main/java/io/sbs/constant/ResultStatus.java
@@ -2,11 +2,11 @@ package io.sbs.constant;
 
 /**
  * @date 2020-03-16
+ * Author : Nanqiao Chen, Anay Paul
  */
 public enum ResultStatus {
 
     SUCCESS("0000", "oporation success"), ERROR("9999", "oporation failed");
-
     private String code;
     private String msg;
 
@@ -15,10 +15,11 @@ public enum ResultStatus {
         this.msg = msg;
     }
 
+    public String getMsg() {
+        return msg;
+    }
     public String getCode() {
         return code;
     }
 
-    public String getMsg() {
-        return msg;
-    }}
+}
