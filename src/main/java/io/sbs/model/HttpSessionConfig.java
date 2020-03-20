d63dc93f1625f5a0033fb0280646b5bac6ecb732
rahulreddy7 <rahulsarikonda@gmail.com>
2020-03-20 13:55:36 -0700

    Mongo Httpsession config

---
+++ src/main/java/io/sbs/model/HttpSessionConfig.java
@@ -0,0 +1,23 @@
+  
+package io.sbs.model;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.mongo.JacksonMongoSessionConverter;
+import org.springframework.session.data.mongo.JdkMongoSessionConverter;
+import org.springframework.session.data.mongo.MongoIndexedSessionRepository;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+
+
+@EnableMongoHttpSession
+public class HttpSessionConfig {
+//	@Bean
+//	public JdkMongoSessionConverter jdkMongoSessionConverter() {
+//		return new JdkMongoSessionConverter(Duration.ofSeconds(900));
+//	}
+	@Bean
+	public JacksonMongoSessionConverter sessionConverter() {
+		return new JacksonMongoSessionConverter();
+	}
+}
