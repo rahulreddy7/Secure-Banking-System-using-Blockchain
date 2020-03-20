0e20e24160cd64bd1389bae733a0d280924e840d
rahulreddy7 <rahulsarikonda@gmail.com>
2020-03-20 13:54:06 -0700

    .

--- src/main/java/io/secure/banking/system/sbsmicroservice/SbsMicroserviceApplication.java
+++ src/main/java/io/secure/banking/system/sbsmicroservice/SbsMicroserviceApplication.java
@@ -1,18 +1,26 @@
 package io.secure.banking.system.sbsmicroservice;
 
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.SpringVersion;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan({"io.sbs.*"})
 @EnableMongoRepositories("io.sbs.*")
 @EnableAutoConfiguration()
-public class SbsMicroserviceApplication {
-
+public class SbsMicroserviceApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SbsMicroserviceApplication.class, args);
 	}
