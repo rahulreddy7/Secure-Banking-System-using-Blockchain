bf6fa1012c8a07c93a4983c105ee0d91403f6c88
Akshay Shah <akshay.vjti11@gmail.com>
2020-03-19 17:44:38 -0700

    removing the logger related code

--- src/main/java/io/sbs/web/advice/ControllerExceptionHandler.java
+++ src/main/java/io/sbs/web/advice/ControllerExceptionHandler.java
@@ -6,8 +6,7 @@ package io.sbs.web.advice;
 
 import io.sbs.exception.BusinessException;
 import io.sbs.vo.ResultVO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,13 +16,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+//    private Logger logger = LoggerFactory.getLogger(getClass());
 
 
     @ExceptionHandler(BusinessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResultVO catchBusinessException(BusinessException e) {
-        logger.error("operation failed：", e);
+        //logger.error("operation failed：", e);
         return ResultVO.createError(e.getMessage());
     }
 
@@ -31,7 +30,7 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResultVO catchException(Exception e) {
-        logger.error("operation failed：", e);
+        //logger.error("operation failed：", e);
         return ResultVO.createError(e.getMessage());
     }
 
