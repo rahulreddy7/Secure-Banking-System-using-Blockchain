2b0bf59180bc642d5a426a3447e0c974283e4846
rahulreddy7 <rahulsarikonda@gmail.com>
2020-03-26 11:51:18 -0700

    Added otp token filter

--- src/main/java/io/sbs/security/JWTAuthenticationFilter.java
+++ src/main/java/io/sbs/security/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package io.sbs.security;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import io.sbs.model.ApplicationUser;
+import io.sbs.model.LoginOTP;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,47 +23,98 @@ import org.springframework.security.core.userdetails.User;
 import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+//public class JWTAuthenticationFilter extends
+//		UsernamePasswordAuthenticationFilter{
+//
+//	private AuthenticationManager authenticationManager;
+//	
+//
+//public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+//		super();
+//		setFilterProcessesUrl("/users/login");
+//		this.authenticationManager = authenticationManager;
+//	}
+//
+////	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+////		this.authenticationManager = authenticationManager;
+////		setFilterProcessesUrl("/users/login");
+////		// TODO Auto-generated constructor stub
+////	}
+//
+//	@Override
+//	public Authentication attemptAuthentication(HttpServletRequest req,
+//			HttpServletResponse res) throws AuthenticationException {
+//		try {
+//			ApplicationUser creds = new ObjectMapper().readValue(req.getInputStream(),
+//					ApplicationUser.class);
+//			return authenticationManager
+//					.authenticate(new UsernamePasswordAuthenticationToken(creds
+//							.getUsername(), creds.getPassword(),
+//							new ArrayList<>()));
+//		} catch (IOException e) {
+//			throw new RuntimeException(e);
+//		}
+//	}
+//
+//	@Override
+//	protected void successfulAuthentication(HttpServletRequest req,
+//			HttpServletResponse res, FilterChain chain, Authentication auth)
+//			throws IOException, ServletException {
+//
+//		String token = JWT
+//				.create()
+//				.withSubject(((User) auth.getPrincipal()).getUsername())
+//				.withExpiresAt(
+//						new Date(System.currentTimeMillis()
+//								+ SecurityConstants.EXPIRATION_TIME))
+//				.sign(HMAC512(SecurityConstants.SECRET.getBytes()));
+//		res.addHeader(SecurityConstants.HEADER_STRING,
+//				SecurityConstants.TOKEN_PREFIX + token);
+//	}
+//
+//}
+
 public class JWTAuthenticationFilter extends
-		UsernamePasswordAuthenticationFilter {
+UsernamePasswordAuthenticationFilter{
 
-	private AuthenticationManager authenticationManager;
+private AuthenticationManager authenticationManager;
 
-	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
-		this.authenticationManager = authenticationManager;
-		setFilterProcessesUrl("/users/login");
-		// TODO Auto-generated constructor stub
-	}
 
-	@Override
-	public Authentication attemptAuthentication(HttpServletRequest req,
-			HttpServletResponse res) throws AuthenticationException {
-		try {
-			ApplicationUser creds = new ObjectMapper().readValue(req.getInputStream(),
-					ApplicationUser.class);
+public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+super();
+setFilterProcessesUrl("/users/otp_check");
+this.authenticationManager = authenticationManager;
+}
 
-			return authenticationManager
-					.authenticate(new UsernamePasswordAuthenticationToken(creds
-							.getUsername(), creds.getPassword(),
-							new ArrayList<>()));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+@Override
+public Authentication attemptAuthentication(HttpServletRequest req,
+	HttpServletResponse res) throws AuthenticationException {
+try {
+	LoginOTP creds = new ObjectMapper().readValue(req.getInputStream(),
+			LoginOTP.class);
+	return authenticationManager
+			.authenticate(new UsernamePasswordAuthenticationToken(creds
+					.getUsername(), creds.getOtp(),
+					new ArrayList<>()));
+} catch (IOException e) {
+	throw new RuntimeException(e);
+}
+}
 
-	@Override
-	protected void successfulAuthentication(HttpServletRequest req,
-			HttpServletResponse res, FilterChain chain, Authentication auth)
-			throws IOException, ServletException {
+@Override
+protected void successfulAuthentication(HttpServletRequest req,
+	HttpServletResponse res, FilterChain chain, Authentication auth)
+	throws IOException, ServletException {
 
-		String token = JWT
-				.create()
-				.withSubject(((User) auth.getPrincipal()).getUsername())
-				.withExpiresAt(
-						new Date(System.currentTimeMillis()
-								+ SecurityConstants.EXPIRATION_TIME))
-				.sign(HMAC512(SecurityConstants.SECRET.getBytes()));
-		res.addHeader(SecurityConstants.HEADER_STRING,
-				SecurityConstants.TOKEN_PREFIX + token);
-	}
+String token = JWT
+		.create()
+		.withSubject(((User) auth.getPrincipal()).getUsername())
+		.withExpiresAt(
+				new Date(System.currentTimeMillis()
+						+ SecurityConstants.EXPIRATION_TIME))
+		.sign(HMAC512(SecurityConstants.SECRET.getBytes()));
+res.addHeader(SecurityConstants.HEADER_STRING,
+		SecurityConstants.TOKEN_PREFIX + token);
+}
 
 }
